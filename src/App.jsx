ce3d33e1de0bd7ca82b2a441a5155f895692cb3b
KHOZEMA SAIFY <saifykhozema89@gmail.com>
2024-08-20 23:56:00 +0530

    addvite

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <div     className="  h-[90vw] w-[90vw] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-blue-200 flex items-center justify-center rounded-full ">
+      <div className=" relative h-5/6 w-10/12 bg-blue-300 rounded-full flex items-center justify-center ">
+        <div className=" relative h-5/6 w-10/12 bg-blue-500 rounded-full flex items-center justify-center  ">
+          <div className="relative h-5/6 w-10/12 bg-blue-600 rounded-full flex items-center justify-center ">
+            <div className="relative h-5/6 w-10/12 bg-blue-700 rounded-full flex items-center justify-center ">
+            <div className="relative h-5/6 w-10/12 bg-blue-800 rounded-full flex items-center justify-center " >
+            <div className="relative h-5/6 w-10/12 bg-blue-900/35 rounded-full flex items-center justify-center " ></div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
